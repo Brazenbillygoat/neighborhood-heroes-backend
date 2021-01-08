8ee5a9cfa8dfd1de8ad8d5843ed475521ba3c8aa
Brazenbillygoat <hyrum.butler3@gmail.com>
2021-01-08 11:28:02 -0500

    No need to include followers and followed as they accomplish the same task by just inluding one so I got rid of followed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def index
     users = User.all
-    render json: users.to_json( {include: [:completed_tasks, :tournaments, :tasks]})
+    render json: users.to_json( {include: [:completed_tasks, :tournaments, :tasks, :followers]})
   end
 
   def show
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
 
     user = User.find_by(username: params["name"])
     if user && user.authenticate(params["password"])
-      render json: user.to_json( {include: [:tasks, :tournaments, :followers, :followed]})
+      render json: user.to_json( {include: [:completed_tasks, :tournaments, :tasks, :followers]})
     else
       render json: "No user found"
     end
